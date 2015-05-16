ee4b8d567937c8dfd58be5fff093a2e2a14ac329
psdewar2 <psdewar2@live.com>
2015-05-16 02:02:23 -0400

    Pulled user data from Parse  for profile info

--- uRyde/Profile.swift
+++ uRyde/Profile.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import Parse
 
 class Profile: UIViewController {
+    
+    //profile info
+    @IBOutlet var username: UILabel!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var email: UILabel!
+    @IBOutlet var phoneNum: UILabel!
+    
+    //get current user
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //DON'T put vars before viewDidLoad
+        //
+        var myself = PFUser.currentUser()
+        var query = PFQuery(className:"_User")
+        if myself != nil {
+            
+            //username
+            let myUserName = myself!.objectForKey("username") as! String
+            username.text = myUserName
+            /*
+            //name
+            let myFirstName = myself!.objectForKey("FirstName")  as! String
+            let myLastName = myself!.objectForKey("LastName")  as! String
+            name.text = "\(myFirstName)" + " " + "\(myLastName)"
+            */
+            //email
+            let myEmail = myself!.objectForKey("email") as! String
+            email.text = myEmail
+            /*
+            //phone number
+            let myPhoneNumber = myself!.objectForKey("phone") as! String
+            phoneNum.text = myPhoneNumber
+            */
+            
+            
+            
+        }
 
     }
     
