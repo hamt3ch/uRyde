0bfdc81503f641dfe3546160ece1fb1f53f7924c
hamt3ch <hmiles23@ufl.edu>
2015-05-16 22:44:27 -0400

    Added extra fields for create Post

--- uRyde/CreatePost.swift
+++ uRyde/CreatePost.swift
@@ -12,29 +12,44 @@ import UIKit
 class CreatePost: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet var myPicker: UIPickerView!
+    @IBOutlet var cityPicker: UIPickerView!
     
-    let pickerData = ["Request","Offer"]
+    @IBOutlet var GasSwitch: UISwitch!
+    let pickerData = ["Cincinnati","Gainesville","Miami","Cleveland"]
     
     @IBOutlet var offerView: UIView!      // OfferView Reference and Fields
-    
-    @IBOutlet var offerDatePicker: UIDatePicker!
+        @IBOutlet var offerDatePicker: UIDatePicker!
     
     
     @IBOutlet var requestView: UIView!  // RequestView Reference and Fields
-    
-    @IBOutlet var requestDatePicker: UIDatePicker!
-    
+        @IBOutlet var requestDatePicker: UIDatePicker!
     
     
-    
-    
+    @IBOutlet var SegementCtrl: UISegmentedControl!
+
     var postSelection = "Request" // initialize to
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myPicker.delegate = self
+        offerDatePicker.datePickerMode = UIDatePickerMode.DateAndTime  // Set UIDatePickers for Date and Time Mode
+        requestDatePicker.datePickerMode = UIDatePickerMode.DateAndTime
+        
+        let currentDate = NSDate()  // setTempVariable to CurrentDate
+        
+        offerDatePicker.minimumDate = currentDate // intialize OfferDatePicker Values
+        offerDatePicker.date = currentDate
+        
+        requestDatePicker.minimumDate = currentDate // intialize OfferDatePicker Values
+        requestDatePicker.date = currentDate
+        
+        SegementCtrl.setTitle("Offer", forSegmentAtIndex: 0) // initialize segCtrl text
+        SegementCtrl.setTitle("Request", forSegmentAtIndex: 1)
+        
+        myPicker.delegate = self     // connect delegates
         myPicker.dataSource = self
+        cityPicker.delegate = myPicker.delegate
+        cityPicker.dataSource = myPicker.dataSource
         
     }
     
@@ -60,8 +75,6 @@ class CreatePost: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         //  myLabel.text = pickerData[row]
-        postSelection = pickerData[row]     // user chooses between Offer or Request
-        showCreatePostView(postSelection) // change subView of CreatePostView
     }
     
     func showCreatePostView(userChoice:String)
@@ -93,6 +106,28 @@ class CreatePost: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
         }
     }
     
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        
+        switch SegementCtrl.selectedSegmentIndex
+        {
+        case 0:
+            showCreatePostView("Offer") // change subView of CreatePostView
+        case 1:
+            showCreatePostView("Request")
+        default:
+            break         }
+    }
 
+    @IBAction func switchTgl(sender: UISwitch) {
+        if(GasSwitch.on)
+        {
+            println("Switch on")
+        }
+        
+        else
+        {
+            println("Switch off")
+        }
+    }
 
 }
